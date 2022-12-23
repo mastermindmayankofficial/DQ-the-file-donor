9c0ca3e839ff70c885e2ed61a3acdb4a2a87b9c9
mastermindmayank <88104889+mastermindmayankofficial@users.noreply.github.com>
2022-12-23 19:41:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -56,7 +56,7 @@ async def is_subscribed(bot, query):
 
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
-        # https://t.me/GetTGLink/4183
+        
         query = (query.strip()).lower()
         title = query
         year = re.findall(r'[1-2]\d{3}$', query, re.IGNORECASE)
@@ -132,7 +132,7 @@ async def get_poster(query, bulk=False, id=False, file=None):
         'rating': str(movie.get("rating")),
         'url':f'https://www.imdb.com/title/tt{movieid}'
     }
-# https://github.com/odysseusmax/animated-lamp/blob/2ef4730eb2b5f0596ed6d03e7b05243d93e3415b/bot/utils/broadcast.py#L37
+
 
 async def broadcast_messages(user_id, message):
     try:
@@ -215,7 +215,7 @@ def get_file_id(msg: Message):
 
 def extract_user(message: Message) -> Union[int, str]:
     """extracts the user from a message"""
-    # https://github.com/SpEcHiDe/PyroGramBot/blob/f30e2cca12002121bad1982f68cd0ff9814ce027/pyrobot/helper_functions/extract_user.py#L7
+    
     user_id = None
     user_first_name = None
     if message.reply_to_message:
