8eea6f6b313bac3f64cfb6028e12386046a6b1e6
mastermindmayank <88104889+mastermindmayankofficial@users.noreply.github.com>
2022-12-24 09:56:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/mastermindmayankofficial/DQ-the-file-donor.git /DQ-the-file-donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Mastermind-Mayank-Movies
+  git clone $UPSTREAM_REPO /DQ-the-file-donor
 fi
-cd /Mastermind-Mayank-Movies
+cd /DQ-the-file-donor
 pip3 install -U -r requirements.txt
 echo "Starting Mastermind-Mayank-Movies...."
 python3 bot.py
