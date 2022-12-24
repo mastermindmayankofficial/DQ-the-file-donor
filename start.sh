ec90f28fffa4673e7d61221c33d03d0ab735e3bb
mastermindmayank <88104889+mastermindmayankofficial@users.noreply.github.com>
2022-12-24 09:47:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mastermindmayankofficial/DQ-the-file-donor/edit/main.git /DQ-the-file-donor
+  git clone https://github.com/mastermindmayankofficial/DQ-the-file-donor.git /DQ-the-file-donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Mastermind-Mayank-Movies
